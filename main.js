fbbd2a9eef6bb1669d557cbc9aa131ad302d7259
enriquepalaciosb <enriquegpalacios@gmail.com>
2022-01-21 13:40:36 -0800

    Added some minor stylings, worked on both functions, and formatted the HTML

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 //Show movies within array
-function showMovie() {
-        console.log(moviesArray);
-        let message = "Title  Year Released Rating\n";
-        for (i = 0; i < moviesArray.length; i++) {
-            message = message + moviesArray[i].oMovieTitle + "  " + moviesArray[i].oYearReleased + "  " + moviesArray[i].oMovieRating + "\n";
-        }
-        document.getElementById("output").value = message;
-}
+    function showMovie() {
+            console.log(moviesArray);
+            let message = "Title  Year Released Rating\n";
+            for (i = 0; i < moviesArray.length; i++) {
+                message = message + moviesArray[i].oMovieTitle + "  " + moviesArray[i].oYearReleased + "  " + moviesArray[i].oMovieRating + "\n";
+            }
+            document.getElementById("output").value = message;
+    }
 // Loading the array
 moviesArray = []; 
 moviesArray.length = 10;
 //Make sure that you tell the user which field is empty
-function addMovie() {
-    let movieTitle = document.getElementById("movieTitle").value.trim();
-    let yearReleased = document.getElementById("yearReleased").value.trim();
-    let userRating = document.getElementById("userRating").value.trim();
-    //VALIDATION 
-    if (movieTitle === '' || yearReleased === '' || userRating === '' ) {
-        alert('Please, fill out the empty fields :)');
-    } else if (userRating >= 6) {
-        alert('Please enter a number between 1 & 5 :)');
-    } else {
-        let MovieReview = {
-            oMovieTitle:  movieTitle,
-            oMovieRating: userRating,
-            oYearReleased: yearReleased,
-        }
-        moviesArray.push(MovieReview);
-    } 
-}
+    function addMovie() {
+        let movieTitle = document.getElementById("movieTitle").value.trim();
+        let yearReleased = document.getElementById("yearReleased").value.trim();
+        let userRating = document.getElementById("userRating").value.trim();
+        //VALIDATION 
+        if (movieTitle === '' || yearReleased === '' || userRating === '' ) {
+            alert('Please, fill out the empty fields :)');
+        } else if (userRating >= 6) {
+            alert('Please enter a number between 1 & 5 :)');
+        } else {
+            // o = object
+            let MovieReview = {
+                oMovieTitle:  movieTitle,
+                oMovieRating: userRating,
+                oYearReleased: yearReleased,
+            }
+            moviesArray.push(MovieReview);
+        } 
+    }

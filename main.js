37574fccfe137f58dea7fdff76beee021379e375
enriquepalaciosb <enriquegpalacios@gmail.com>
2022-01-23 12:52:27 -0800

    Worked on the validation and made the fields blank for the user to add in ten flicks

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 let moviesArray = []; 
 //Show movies within array
     function showMovie() {
-            let message = "Title  Year Released Rating\n";
+            let message = "Title \t \t Year Released \t \t Rating\n";
             for (i = 0; i < moviesArray.length; i++) {
-                message = message + moviesArray[i].title + "  " + moviesArray[i].year + "  " + moviesArray[i].rating + "\n";
+                message = message + moviesArray[i].title + "\t \t" + moviesArray[i].year + "\t \t" + moviesArray[i].rating + "\n";
             }
             document.getElementById("show").value = message;
     }
@@ -17,13 +17,22 @@ let moviesArray = [];
         let movieTitle = document.getElementById("title").value.trim();
         let yearReleased = document.getElementById("year").value.trim();
         let userRating = document.getElementById("rating").value.trim();
+        //Creates a blank so the user can enter up to ten flicks
+        document.getElementById("title").value = "";
+        document.getElementById("year").value = "";
+        document.getElementById("rating").value = "";
         //VALIDATION 
         if (movieTitle === "" || yearReleased === "" || userRating === "" ) {
-            alert('Please, fill out the empty fields :)');
+            alert("Please, fill out the empty fields :)");
+        } else if (movieTitle === "") {
+            alert("Please, enter a title :)");
+        } else if (yearReleased === "") {
+            alert("Please, enter the year :)");
+        } else if (userRating === "") {
+        alert("Please, enter your rating :)");
         } else if (userRating >= 6) {
-            alert('Please enter a number between 1 & 5 :)');
+            alert("Please enter a number between 1 & 5 :)");
         } else {
-            // o = object
             let Movie = {
                 title: movieTitle,
                 rating: userRating,
